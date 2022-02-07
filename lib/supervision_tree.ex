efc581cd41e9d0cbe834f76c3646f8f371df7ee8
Georgiy Bykov <zhora.bykov@gmail.com>
2022-02-08 00:30:00 +0300

    Initial commit

---
+++ lib/supervision_tree.ex
@@ -0,0 +1,18 @@
+defmodule SupervisionTree do
+  @moduledoc """
+  Documentation for `SupervisionTree`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SupervisionTree.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/supervision_tree_test.exs
@@ -0,0 +1,8 @@
+defmodule SupervisionTreeTest do
+  use ExUnit.Case
+  doctest SupervisionTree
+
+  test "greets the world" do
+    assert SupervisionTree.hello() == :world
+  end
+end
